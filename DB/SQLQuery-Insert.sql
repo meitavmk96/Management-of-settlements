ad8a00e60528fa4d67014b6ae8be9ab0c1c93603
meitavmk96 <meitavmk96@gmail.com>
2024-02-09 16:38:18 +0200

    Version 1

---
+++ DB/SQLQuery-Insert.sql
@@ -0,0 +1,39 @@
+﻿-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<meitav>
+-- Create date: <05/02/24>
+-- Description:	<Insert a settlements>
+-- =============================================
+CREATE PROCEDURE spInsertSettlement
+	-- Add the parameters for the stored procedure here
+	@name nvarchar (30),
+	@insertedId int OUTPUT
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	--SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	Insert INTO [Settlements] ([name]) VALUES (@name)
+	 SET @insertedId = SCOPE_IDENTITY()
+END
+GO
+
+--exec spInsertSettlement N'רעננה'
+--Select * from [Settlements]
+--DROP PROCEDURE spInsertSettlement;
---
+++ Server/Server/Models/Settlement.cs
@@ -0,0 +1,55 @@
+﻿using static DBservices;
+
+namespace Server.Models
+{
+    public class Settlement
+    {
+        //fileds
+        int id;
+        string name;
+
+        //properties
+        public string Name { get => name; set => name = value; }
+        public int Id { get => id; set => id = value; }
+
+
+        public Settlement(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public Settlement() { }
+
+        public List<Settlement> Read()
+        {
+            DBservices dbs = new DBservices();
+            return dbs.Read_Settlements();
+        }
+
+        public InsertResult Insert(out int id)
+        {
+            DBservices dbs = new DBservices();
+            return dbs.Insert_Settlement(this, out id);
+        }
+
+        public InsertResult Update()
+        {
+            DBservices dbs = new DBservices();
+            return dbs.Update_Settlement(this);
+        }
+
+        public int Delete(int id)
+        {
+            DBservices dbs = new DBservices();
+            List<Settlement> SettlementsList = dbs.Read_Settlements();
+
+            foreach (Settlement settlement in SettlementsList)
+            {
+                if (settlement.Id == id)
+                    return dbs.Delete_Settlement(id);
+            }
+            return (0);
+        }
+    }
+}
